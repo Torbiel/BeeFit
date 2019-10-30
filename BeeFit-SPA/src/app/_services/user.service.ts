68671a4341babd831ca487563d432d2f1c157cd1
Torbiel <syzmon44@gmail.com>
2019-10-30 13:50:14 +0100

    q

--- BeeFit-SPA/src/app/_services/user.service.ts
+++ BeeFit-SPA/src/app/_services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/User';
 
@@ -10,7 +10,7 @@ import { User } from '../_models/User';
 export class UserService {
   baseUrl = environment.apiUrl + 'users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.baseUrl);
