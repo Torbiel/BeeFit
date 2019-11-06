a5d747f681c490a7c55b135dac6a3ad923aa207e
Torbiel <syzmon44@gmail.com>
2019-11-06 20:57:40 +0100

    Ingredient model updated

---
+++ BeeFit.API/Migrations/20191106195213_IngredientsUpdate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeeFit.API.Migrations
+{
+    public partial class IngredientsUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Ingredients",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GramsPerUnit",
+                table: "Ingredients",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "GramsPerUnit",
+                table: "Ingredients");
+        }
+    }
+}
