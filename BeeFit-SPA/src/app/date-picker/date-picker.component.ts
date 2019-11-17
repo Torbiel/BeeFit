36d84f92d9e2362882d12c2fb234e3cbe2140471
Radosław Lipski <rlipski76@gmail.com>
2019-11-17 16:43:08 +0100

    DatePicker

--- BeeFit-SPA/src/app/date-picker/date-picker.component.ts
+++ BeeFit-SPA/src/app/date-picker/date-picker.component.ts
@@ -11,73 +11,59 @@ export class DatePickerComponent implements OnInit {
   now: Date;
   selectedDate: Date;
   localeString: string = 'en';
-  visibleDates: Array<{ date: Date, index:number }> = [];
+  visibleDates: Array<{ date: Date, index: number }> = [];
   subscribedParam = "initial value";
 
   constructor(private route: ActivatedRoute) {
 
     this.now = new Date();
     this.selectedDate = this.now;
-    this.selectedDate.setDate(this.selectedDate.getDate());;
-    console.log(this.selectedDate);
+    this.prepareWeek();
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      this.subscribedParam = params.get("date");
+      this.subscribedParam = params.get("date"); console.log(this.subscribedParam);
     });
-    this.prepareWeek();
-
   }
-
-  // ngAfterViewInit() {
-
-  //   var dateItems = document.querySelectorAll<HTMLElement>('.carousel-list li'), i;
-
-  //   for (i = 0; i < dateItems.length; ++i) {
-  //     dateItems[i].classList.add('carousel-seat');
-
-  //   }
-  //   dateItems[dateItems.length - 1].classList.add('is-ref');
-  // }
 
   prepareWeek() {
     var weekDaySelected = this.selectedDate.getDay();
-    if (!weekDaySelected) {
-      weekDaySelected = 7;
-    }
-    for(let i=0; i < 9; i++) {
+    for (let i = 1; i <= 9; i++) {
       var weekDay = weekDaySelected - i;
-      this.visibleDates[i-1] = {date:  new Date(), index: i-1};
-      this.visibleDates[i-1].date.setDate(this.selectedDate.getDate() - weekDay-1);
+      this.visibleDates[i - 1] = { date: new Date(), index: i - 1 };
+      this.visibleDates[i - 1].date.setDate(this.selectedDate.getDate() - weekDay - 1);
     };
-
-
   }
 
   slideDate(direction: string) {
-    // console.log(this.visibleDates);
-    // let datesList = document.querySelector('.carousel-list');
-    // var dateItem = document.querySelector('.date-item');
-    // var listPosition = parseFloat(window.getComputedStyle(datesList).getPropertyValue("left"));
 
+    var datesList = document.querySelector('.carousel-list');
+    var dateItem = document.querySelector('.date-item');
+
+    var listPosition = parseFloat(window.getComputedStyle(datesList).getPropertyValue("left"));
+    var dateItemWidth = parseFloat(window.getComputedStyle(dateItem).getPropertyValue("width"));
+
+    if (direction == 'forward') {
+      listPosition = listPosition - dateItemWidth;
+    } else {
+      //listPosition = listPosition + dateItemWidth;
+    }
+
+    (datesList as HTMLElement).style.left = (listPosition) + 'px';
     var sizeOfList = this.visibleDates.length;
 
     if (direction == 'forward') {
-      for (let i = 0; i < sizeOfList; i++) {
-        this.visibleDates[i].date.setDate(this.visibleDates[i].date.getDate() + 1);
-        console.log(this.visibleDates[i]);
-      }
-      // this.visibleDates[sizeOfList] = Object.assign({}, this.visibleDates[sizeOfList - 1]);
-      // this.visibleDates[sizeOfList].date = new Date(this.visibleDates[sizeOfList - 1].date.setDate(this.visibleDates[sizeOfList - 1].date.getDate() + 1));
+      this.visibleDates[sizeOfList] = Object.assign({}, this.visibleDates[sizeOfList - 1]);
+
+      this.visibleDates[sizeOfList].date = new Date(this.visibleDates[sizeOfList - 1].date.setDate(this.visibleDates[sizeOfList - 1].date.getDate() + 1));
     } else {
-      for (let i = 0; i < sizeOfList; i++) {
-        this.visibleDates[i].date.setDate(this.visibleDates[i].date.getDate() - 1);
-      }
+      this.visibleDates.unshift(Object.assign({}, this.visibleDates[0]));
+      this.visibleDates[0].date = new Date(this.visibleDates[1].date.setDate(this.visibleDates[1].date.getDate() - 1));
     }
 
-   //this.visibleDates[sizeOfList].index = sizeOfList;
-  // sizeOfList++;
+    this.visibleDates[sizeOfList].index = sizeOfList;
+    sizeOfList++;
   }
 
 }
