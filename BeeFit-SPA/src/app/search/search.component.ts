9b8c3809b92b74a4761de30285ce519668b390dc
Radosław Lipski <rlipski76@gmail.com>
2019-12-04 10:57:02 +0100

    Radio selector

--- BeeFit-SPA/src/app/search/search.component.ts
+++ BeeFit-SPA/src/app/search/search.component.ts
@@ -19,6 +19,7 @@ export class SearchComponent implements OnInit {
     this.filterParams.userId = null;
     this.filterParams.minCallories = null;
     this.filterParams.maxCallories = null;
+    this.selectRadio();
   }
 
   search() {
@@ -34,5 +35,33 @@ export class SearchComponent implements OnInit {
     this.filterParams.minCallories = null;
     this.filterParams.maxCallories = null;
     this.filtersReset.emit(this.filterParams);
+  }
+
+  selectRadio(radioGroupName?: string) {
+
+    var radioGroup = document.querySelector('#' + radioGroupName); console.log(radioGroup);
+
+    var radioGroupElements = Array.from(radioGroup.querySelectorAll('div'));
+
+    radioGroupElements[0].querySelector('input').setAttribute("checked", "checked");
+
+    var frame = <HTMLElement>document.querySelector('.' + radioGroupName);
+    frame.style.width = radioGroupElements[0].offsetWidth+'px';
+
+    for (let element of radioGroupElements) {
+      element.querySelector('input').style.display='none';
+      if (element.querySelector('input').checked) {
+        element.querySelector('label').style.color = '#000';
+        var newLeftPosition = element.offsetLeft +
+         parseInt(window.getComputedStyle(element).paddingLeft, 10) +
+         parseInt(window.getComputedStyle(element.querySelector('label')).marginLeft, 10);
+        var newWidth = element.querySelector('label').getBoundingClientRect().width;
+        frame.style.left = newLeftPosition + 'px';
+        frame.style.width = newWidth + 'px';
+      } else {
+        element.querySelector('label').style.color = '#fff';
+      }
+    }
+
   }
 }
